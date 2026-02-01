e32913c25c6c9b969df080733ffa5112922c15eb
Usama Hashmi <hash.design.develop@gmail.com>
2026-02-01 11:06:13 -0600

    Initial commit

---
+++ Calculator.Tests/CalcTests.cs
@@ -0,0 +1,7 @@
+﻿namespace Calculator.Tests;
+
+public class CalcTests
+{
+    [Fact]
+    public void Add_Works() => Assert.Equal(5, Calc.Add(2, 3));
+}
---
+++ Calculator/Calc.cs
@@ -0,0 +1,6 @@
+﻿namespace Calculator;
+
+public static class Calc
+{
+    public static int Add(int a, int b) => a + b;
+}
